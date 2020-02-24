bc4dbf89e01e5776e36837fb8665d86f7a1290d7
MinBao <1341682186@qq.com>
2020-02-24 18:55:38 +0800

    安装paddle

---
+++ 识别数字.py
@@ -0,0 +1,15 @@
+#加载飞桨和相关类库
+import paddle
+import paddle.fluid as fluid
+#from paddle import fluid
+from paddle.fluid.dygraph.nn import FC
+import numpy as np
+import os
+from PIL import Image
+
+
+# 如果～/.cache/paddle/dataset/mnist/目录下没有MNIST数据，API会自动将MINST数据下载到该文件夹下
+# 设置数据读取器，读取MNIST数据训练集
+trainset = paddle.dataset.mnist.train()
+# 包装数据读取器，每次读取的数据数量设置为batch_size=8
+train_reader = paddle.batch(trainset, batch_size=8)
